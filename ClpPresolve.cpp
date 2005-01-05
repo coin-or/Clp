49f3db7c67c0ae33e9bc2e2e0371e8d3c058846e
John Forrest <john.forrest@fastercoin.com>
2005-01-05 19:52:24 +0000

    add presolveOptions_=0

--- ClpPresolve.cpp
+++ ClpPresolve.cpp
@@ -972,7 +972,8 @@ CoinPresolveMatrix::CoinPresolveMatrix(int ncols0_in,
   rowsToDo_(new int [nrows_in]),
   numberRowsToDo_(0),
   nextRowsToDo_(new int[nrows_in]),
-  numberNextRowsToDo_(0)
+  numberNextRowsToDo_(0),
+  presolveOptions_(0)
 
 {
   const int bufsize = 2*nelems_in;
