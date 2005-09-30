fa69ec785fd4b261608914fd542959482742f4a0
John Forrest <john.forrest@fastercoin.com>
2005-09-30 18:50:59 +0000

    licence

--- include/ClpCholeskyTaucs.hpp
+++ include/ClpCholeskyTaucs.hpp
@@ -9,27 +9,11 @@ class ClpMatrixBase;
 
 /** Taucs class for Clp Cholesky factorization
 
-Taucs comes with no warranty whatsoever and is distributed under the GNU LGPL (Library or 
-Lesser GNU Public Library). The license is available in www.gnu.org. Alternatively, you can also 
-elect to use Taucs under the following UMFPACK-style license, which is simpler to understand 
-than the LGPL: 
-Taucs. Version 1.0, November 29, 2001. Copyright (c) 2001 by Sivan Toledo, Tel-Aviv
-Univesity, stoledo@tau.ac.il. All Rights Reserved.
-TAUCS License:
-Your use or distribution of TAUCS or any derivative code implies that you agree to
-this License OR to the GNU LGPL.
-THIS MATERIAL IS PROVIDED AS IS, WITH ABSOLUTELY NO WARRANTY
-EXPRESSED OR IMPLIED. ANY USE IS AT YOUR OWN RISK.
-Permission is hereby granted to use or copy this program, provided that the Copyright,
-this License, and the Availability of the original version is retained on all copies. User
-documentation of any code that uses this code or any derivative code must cite the
-Copyright, this License, the Availability note, and"Used by permission."If this code or
-any derivative code is accessible from within M....., then typing "help taucs" must
-cite the Copyright, and "type taucs" must also cite this License and the Availability
-note. Permission to modify the code and to distribute modified code is granted,
-provided the Copyright, this License, and the Availability note are retained, and a
-notice that the code was modified is included. This software is provided to you free
-of charge.
+If  you wish to use Sivan Toledo's TAUCS code see
+
+http://www.tau.ac.il/~stoledo/taucs/
+
+for terms of use
 
 The taucs.h file was modified to put 
 
