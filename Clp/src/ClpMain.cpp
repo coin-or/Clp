ec71a9b5a692e5ea7a1655573b944f324128db77
John Forrest <john.forrest@fastercoin.com>
2007-06-27 10:27:37 +0000

    say version 1.4

--- Clp/src/ClpMain.cpp
+++ Clp/src/ClpMain.cpp
@@ -15,7 +15,7 @@
 #include "CoinHelperFunctions.hpp"
 #include "CoinSort.hpp"
 // History since 1.0 at end
-#define CLPVERSION "1.03.03"
+#define CLPVERSION "1.04.01"
 
 #include "CoinMpsIO.hpp"
 #include "CoinFileIO.hpp"
@@ -2192,4 +2192,5 @@ static void generateCode(const char * fileName,int type)
   1.02.02 May 19 2005.  Stuff for strong branching and some improvements to simplex
   1.03.01 May 24 2006.  Lots done but I can't remember what!
   1.03.03 June 13 2006.  For clean up after dual perturbation
+  1.04.01 June 27 2007.  Lots of changes but I got lazy
  */
