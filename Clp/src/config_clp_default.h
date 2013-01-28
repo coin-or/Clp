aea149d0b7bf5b04febb059cf371e34befb7c45e
Stefan Vigerske <svigerske@gams.com>
2013-01-28 15:42:10 +0000

    restoring Clp/trunk

--- Clp/src/config_clp_default.h
+++ Clp/src/config_clp_default.h
@@ -5,13 +5,13 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define CLP_VERSION "1.15"
+#define CLP_VERSION      "trunk"
 
 /* Major Version number of project */
-#define CLP_VERSION_MAJOR 1
+#define CLP_VERSION_MAJOR   9999
 
 /* Minor Version number of project */
-#define CLP_VERSION_MINOR 15
+#define CLP_VERSION_MINOR   9999
 
 /* Release Version number of project */
 #define CLP_VERSION_RELEASE 9999
