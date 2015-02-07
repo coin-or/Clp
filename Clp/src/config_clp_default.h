010a85776fc4aa9f8d8ef3a4d314ddb93f8d937c
Ted Ralphs <ted@lehigh.edu>
2015-02-07 04:46:12 +0000

    restoring Clp/stable/1.16

--- Clp/src/config_clp_default.h
+++ Clp/src/config_clp_default.h
@@ -5,7 +5,7 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define CLP_VERSION "1.16.1"
+#define CLP_VERSION "1.16"
 
 /* Major Version number of project */
 #define CLP_VERSION_MAJOR 1
@@ -14,4 +14,4 @@
 #define CLP_VERSION_MINOR 16
 
 /* Release Version number of project */
-#define CLP_VERSION_RELEASE 1
+#define CLP_VERSION_RELEASE 9999
