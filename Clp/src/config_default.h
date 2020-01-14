aae43fd66318fe5a17d0be3cbf149f556bd0834b
Stefan Vigerske <svigerske@gams.com>
2020-01-14 13:48:54 +0000

    add CLP_EXPORT in config-default.h

--- Clp/src/config_default.h
+++ Clp/src/config_default.h
@@ -2,6 +2,16 @@
 /* include the COIN-OR-wide system specific configure header */
 #include "configall_system.h"
 
+/* this needs to come before the include of config_clp_default.h */
+#ifndef CLP_EXPORT
+#ifdef _WIN32
+/* assuming we build a CoinUtils DLL */
+#define CLP_EXPORT __declspec(dllexport)
+#else
+#define CLP_EXPORT
+#endif
+#endif
+
 /* include the public project specific macros */
 #include "config_clp_default.h"
 
@@ -10,17 +20,8 @@
 /*    These are only in effect in a setting that doesn't use configure     */
 /***************************************************************************/
 
-/* Define to the debug sanity check level (0 is no test) */
-#define COIN_CLP_CHECKLEVEL 0
-
-/* Define to the debug verbosity level (0 is no output) */
-#define COIN_CLP_VERBOSITY 0
-
 /* Define to 1 if the CoinUtils package is used */
 #define COIN_HAS_COINUTILS 1
-
-/* Define to 1 if Clp is used */
-#define COIN_HAS_CLP 1
 
 /* vi: softtabstop=2 shiftwidth=2 expandtab tabstop=2
 */
